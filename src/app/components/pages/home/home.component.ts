d3a234c0e700d29b7b1874675999aca42b2d9f76
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-04-24 20:50:01 +0100

    Home page done

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Moment } from 'src/app/moment';
+import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  baseApiUrl = environment.baseApiUrl;
+  allMoments: Moment[] = [];
+  filteredMoments: Moment[] = [];
+
+  constructor(private momentService: MomentService) { }
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((resp) => {
+      const data = resp.data;
+
+      data.map((moment) => {
+        moment.created_at = new Date(moment.created_at!).toLocaleDateString(
+          'en-us'
+        );
+      });
+
+      this.allMoments = data;
+      this.filteredMoments = data;
+    });
+  }
+}
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -5,15 +5,20 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 import { Moment } from 'src/app/moment';
+import { Response } from 'src/app/response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl;
-  private momentApiUrl = `${this.baseApiUrl}/moments`;
+  private momentApiUrl = `${this.baseApiUrl}/api/moments`;
 
   constructor(private httpClient: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.httpClient.get<Response<Moment[]>>(this.momentApiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.httpClient.post<FormData>(this.momentApiUrl, formData);
