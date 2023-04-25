ecb8211fae694d474eb8d80b670c7067fad492d1
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-04-25 10:04:49 +0100

    feat: Serch for moments

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import { Moment } from 'src/app/moment';
 import { MomentService } from 'src/app/services/moment.service';
@@ -13,6 +14,11 @@ export class HomeComponent implements OnInit {
   baseApiUrl = environment.baseApiUrl;
   allMoments: Moment[] = [];
   filteredMoments: Moment[] = [];
+
+  faSearch = faSearch;
+  placeholder = 'Search for a moment...'
+
+  searchText = '';
 
   constructor(private momentService: MomentService) { }
 
@@ -29,5 +35,12 @@ export class HomeComponent implements OnInit {
       this.allMoments = data;
       this.filteredMoments = data;
     });
+  }
+
+  handleSearch() {
+    this.filteredMoments = this.allMoments.filter((moment) => {
+      return moment.title.toUpperCase().includes(this.searchText.toUpperCase());
+    })
+
   }
 }
